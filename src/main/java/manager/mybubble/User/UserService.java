9791e2c8d507a7289e46762a0e5e96734d7dffcb
YannSTEFANUTTI <stefanutti.yann@gmail.com>
2023-06-20 09:18:50 +0200

    environnement variable

--- src/main/java/manager/mybubble/User/UserService.java
+++ src/main/java/manager/mybubble/User/UserService.java
@@ -18,7 +18,7 @@ public class UserService {
     }
 
     public User getById(Long id) {
-        return userRepository.findById(id);
+        return userRepository.findById(id).orElseThrow(()-> new RuntimeException(("User"+ id + "not found")));
     }
 
     public User add(User user) {
